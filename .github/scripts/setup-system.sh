34ad65bb836f66e1d52d88de97da48ef1b4e7332
Oscar Beaumont <oscar@otbeaumont.me>
2022-05-05 05:52:43 +0800

    full send

--- .github/scripts/setup-system.sh
+++ .github/scripts/setup-system.sh
@@ -15,40 +15,38 @@ if [ $? -eq 1 ]; then
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if (( $EUID != 0 )); then
+                echo "This script must be run as root!"
+                exit
+        fi
+
         if which apt-get &> /dev/null; then
                 echo "Detected 'apt' based distro!"
                 DEBIAN_TAURI_DEPS="libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libappindicator3-dev librsvg2-dev" # Tauri dependencies
                 DEBIAN_FFMPEG_DEPS="libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavresample-dev libavutil-dev libswscale-dev libswresample-dev ffmpeg" # FFMPEG dependencies
                 DEBIAN_BINDGEN_DEPS="pkg-config clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
 
-                sudo apt-get -y update
-                sudo apt-get -y install $DEBIAN_TAURI_DEPS $DEBIAN_FFMPEG_DEPS $DEBIAN_BINDGEN_DEPS
-        elif which pacman &> /dev/null; then
+                apt-get -y update
+                apt-get -y install $DEBIAN_TAURI_DEPS $DEBIAN_FFMPEG_DEPS $DEBIAN_BINDGEN_DEPS
+        elif whereis pacman &> /dev/null; then
                 echo "Detected 'pacman' based distro!"
-                 sudo pacman -S --needed webkit2gtk base-devel curl wget openssl appmenu-gtk-module gtk3 libappindicator-gtk3 librsvg libvips
-
-                ARCH_TAURI_DEPS="libwebkit2gtk-4.0-dev build-essential curl wget libssl-dev libgtk-3-dev libappindicator3-dev librsvg2-dev" # Tauri dependencies
-                ARCH_FFMPEG_DEPS="" # FFMPEG dependencies # TODO
+                ARCH_TAURI_DEPS="webkit2gtk base-devel curl wget openssl appmenu-gtk-module gtk3 libappindicator-gtk3 librsvg libvips" # Tauri dependencies
+                ARCH_FFMPEG_DEPS="ffmpeg" # FFMPEG dependencies
                 ARCH_BINDGEN_DEPS="clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
 
-                sudo pacman -Syu
-                sudo pacman -S --needed $ARCH_TAURI_DEPS $ARCH_FFMPEG_DEPS $ARCH_BINDGEN_DEPS
-
-                # TODO: Remove warning
-                echo "The FFMPEG dependencies are not yet included in this script for your Linux Distro. Please install them manually."
-                echo "It would also be greatly appreciated if you could ping @oscartbeaumont in the Discord or GitHub so that you can help me work these out and update the script for everyone."
-                exit 1
+                pacman -Syu
+                pacman -S --needed $ARCH_TAURI_DEPS $ARCH_FFMPEG_DEPS $ARCH_BINDGEN_DEPS
         elif which dnf &> /dev/null; then
                 echo "Detected 'dnf' based distro!"
                 FEDORA_TAURI_DEPS="webkit2gtk3-devel.x86_64 openssl-devel curl wget libappindicator-gtk3 librsvg2-devel" # Tauri dependencies
                 FEDORA_FFMPEG_DEPS="ffmpeg ffmpeg-devel" # FFMPEG dependencies
                 FEDORA_BINDGEN_DEPS="clang" # Bindgen dependencies - it's used by a dependency of Spacedrive
 
-                sudo dnf check-update
-                sudo dnf install $FEDORA_TAURI_DEPS $FEDORA_FFMPEG_DEPS $FEDORA_BINDGEN_DEPS
-                sudo dnf group install "C Development Tools and Libraries"
+                dnf check-update
+                dnf install $FEDORA_TAURI_DEPS $FEDORA_FFMPEG_DEPS $FEDORA_BINDGEN_DEPS
+                dnf group install "C Development Tools and Libraries"
         else
-                echo "Your Linux distro '$(lsb_release -s -d)' is not supported by this script. We would welcome a PR or some help adding your OS to this script. https://github.com/spacedriveapp/spacedrive/issues"
+                echo "Your Linux distro '$(lsb_release -s -d | echo "Unknown")' is not supported by this script. We would welcome a PR or some help adding your OS to this script. https://github.com/spacedriveapp/spacedrive/issues"
                 exit 1
         fi
 
